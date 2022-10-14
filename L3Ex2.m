73d347b0e3a38ff2ba4c70d362e4b3c1732c7b23
Wesley Gonçalves da Silva <115840128+wesleygds-tecnico@users.noreply.github.com>
2022-10-14 23:15:27 +0100

    Add files via upload

---
+++ L3Ex2.m
@@ -0,0 +1,48 @@
+function [] = L3Ex2(A,B)
+%Determinante da matriz é -1.8504e-16
+D = A;
+n = length(A);
+for j = 1:(n-1)
+    for i = (j+1):n
+        if (A(j,j) == 0)
+            C = A(j,:);
+            A(j,:) = A(j+1,:);
+            A(j+1,:) = C;
+            C = B(j,1);
+            B(j,1) = B(j+1,1);
+            B(j+1,1) = C;
+        else
+            m = A(i,j)/A(j,j);
+            A(i,j) = A(i,j) - A(j,j)*m;
+            for k = (j+1):n
+                A(i,k) = A(i,k) - A(j,k)*m; 
+            end
+            B(i) = B(i) - B(j)*m;
+         end
+     end
+end
+fprintf('O sistema equivalente é definido por: \n');
+disp(A)
+disp(B)
+if(det(A) == 0)
+    fprintf('O sistema não possuí solução \n')
+else
+x = zeros(n,1);
+x(n) = B(n)/(A(n,n));
+for i = (n-1):-1:1 
+    soma = 0;
+    for j = (i+1):n
+        soma = soma + x(j)*A(i,j);
+    end
+    x(i) = (B(i) - soma)/A(i,i);
+end
+fprintf('O resultado do sistema é: \n')
+disp(x)
+fprintf('Verificação \n')
+C = D*x;
+disp(C)
+C = -det(A);
+fprintf('A determinante da matriz triangular é: %d \n', C)
+end
+end
+
